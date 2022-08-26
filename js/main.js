27bd258ed2e63c56c1350de7334bec25e950b60b
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-08-26 15:53:51 -0500

    html template done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-// main content for weather app goes here
+// // main content for weather app goes here
+
+// Using the Open Weather API create a weather application that, at a minimum, displays the High, Low, Current and Feels Like using JavaScript.
+// (Creative freedom is encouraged here)
+// You will possibly need to change the weather data from Kelvin to Farenheit using this formula (Depending on the api endpoint you are accessing)
+// ℉=((K-273.15)*1.8)+32
+// You can add a zip code to your project if you wish (not a strict requirement). This is shown in the walkthrough demo video as a possible outcome but again is not mandatory.
+// Once the project is complete, commit the project to github and submit the github repository link to the assignment.
+
+console.log('in main.js')
+console.log(APIKey)
+
+// displays the High, Low, Current and Feels Like using JavaScript.
+// so i let a 'p' element within each of the 4 weather cards. i can do htmlinner to insert text into those elements
